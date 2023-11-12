f52c4ef9c1fd09171ab53db051f62f9bd0265b25
Piarre <pierre@ideestore.fr>
2023-11-12 10:16:31 +0100

    Refactor code to use Commander instead of Clack prompts and update Git utils.

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,10 @@
-import { confirm, intro, outro, select, spinner } from "@clack/prompts";
-import chalk from "chalk";
-import { commandList } from "./config.js";
+import { Command, createCommand } from "commander";
+import Git from "./utils/git.js";
 
-/** Local imports */
-import { Git } from "./utils/git.js";
-import checkForNewVersion from "./utils/checkNewVersion.js";
-import { AVAILABLE_COMMANDS, HELP } from "./config.js";
+const program = new Command();
+program.version("1.0.0");
 
-(async (args: any) => {
-  const command = args[0] as string;
+program.command("add").action(() => Git.getFilesToAdd());
+program.command("commit").action(() => Git.commit());
 
-  // await checkForNewVersion();
-  await Git.checkForRepository();
-
-  if (!command) {
-    const cmd = await select({
-      message: "Tests",
-      options: commandList.map((cmd: any) => {
-        return {
-          name: cmd,
-          value: (cmd as string).charAt(0).toUpperCase() + cmd.slice(1),
-        };
-      }) as any,
-      initialValue: "help",
-      maxItems: 1,
-    });
-
-    if (cmd.toString().toLowerCase() == AVAILABLE_COMMANDS.ADD) {
-      await Git.getFilesToAdd();
-    }
-
-    if (cmd.toString().toLowerCase() == AVAILABLE_COMMANDS.COMMIT) {
-      await Git.commit();
-    }
-
-    if (cmd.toString().toLowerCase() == AVAILABLE_COMMANDS.HELP) {
-      outro(HELP);
-      process.exit(0);
-    }
-  }
-
-  if (command == AVAILABLE_COMMANDS.ADD) {
-    await Git.getFilesToAdd();
-  }
-
-  if (command == AVAILABLE_COMMANDS.COMMIT) {
-    await Git.commit();
-  }
-
-  if (command == AVAILABLE_COMMANDS.HELP) {
-    intro(HELP);
-    process.exit(0);
-  }
-})(process.argv.slice(2));
+program.parse(process.argv);
