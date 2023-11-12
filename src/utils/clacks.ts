77e3ad90d939224827bed9abcb38db6702a6845a
Piarre <pierre@ideestore.fr>
2023-11-12 16:11:54 +0100

    Custom message on Cancel()

--- src/utils/clacks.ts
+++ src/utils/clacks.ts
@@ -1,9 +1,9 @@
 import { isCancel, outro } from "@clack/prompts";
 import chalk from "chalk";
 
-export function Cancel(event: unknown) {
+export function Cancel(event: unknown, message?: string) {
   if (isCancel(event)) {
-    outro(chalk.red("Exited"));
+    outro(chalk.red(message || "Exited"));
     process.exit(0);
   }
 }
