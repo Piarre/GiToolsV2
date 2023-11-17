b1d0190911786ea2fa653b0d049df9dd2333f5e9
Piarre <pierre@ideestore.fr>
2023-11-17 19:01:41 +0100

    Check for errors on version check

--- src/utils/checkNewVersion.ts
+++ src/utils/checkNewVersion.ts
@@ -4,13 +4,16 @@ import { gitools } from "../index.js";
 
 export default async function chechForNewVersion() {
   // Get version tag from npmjs.com
-  const remoteVersion = await fetch("https://registry.npmjs.com/@piarre/gitools/")
+  await fetch("https://registry.npmjs.com/@piarre/gitools/")
     .then((res) => res.json())
-    .then((res) => res["dist-tags"].latest);
-
-  if (remoteVersion != !gitools.version()) {
-    intro(chalk.green(`A new version is available: ${remoteVersion}`));
-    outro(chalk.green(`Update by running: npm i -g gitools@${remoteVersion}`));
-  }
+    .then((res) => {
+      const newVer = res["dist-tags"].latest;
+      if (newVer != !gitools.version()) {
+        intro(chalk.green(`A new version is available: ${newVer}`));
+        outro(chalk.green(`Update by running: npm i -g gitools@${newVer}`));
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
-
