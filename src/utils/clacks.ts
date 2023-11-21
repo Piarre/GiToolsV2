90ce7ca872113a993261840c0d5252310e1cc872
Piarre <pierre@ideestore.fr>
2023-11-21 15:12:00 +0100

    previous commit mdrrrr

--- src/utils/clacks.ts
+++ src/utils/clacks.ts
@@ -1,9 +1,19 @@
 import { isCancel, outro } from "@clack/prompts";
 import chalk from "chalk";
+import { gitmojis } from "gitmojis";
 
 export function Cancel(event: unknown, message?: string) {
   if (isCancel(event)) {
-    outro(chalk.red(message || "Exited"));
+    outro(`🔥 ${chalk.red(message || "Exited")}`);
     process.exit(0);
   }
+}
+
+export function Exit(message: string) {
+  outro(`${gitmojis} ${chalk.red(message)}`);
+  process.exit(0);
+}
+
+export function message(message: string, /** emoji: string, */ color?: string) {
+  return chalk.hex(color || "#ff8c00")(message);
 }
